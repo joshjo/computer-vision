8aee21651dfce13e2a8eeda02e2744339e60243c
LizLima <lizbethlima@gmail.com>
2018-12-03 17:52:20 -0500

    Actualizacion de parametros de circularidad

--- Calibracion/main.cpp
+++ Calibracion/main.cpp
@@ -2,15 +2,17 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 #include <iostream>
- 
+#include <cmath>
 using namespace std;
 using namespace cv;
+
+const double PI  =3.141592653589793238463;
 
 Mat process (Mat obj);
 int main( ) {
   
 /*
-    Mat image = imread("/home/liz/MEGAsync/Semestre-II/Imagenes-Loayza/Calibracion/sources/prueba.png", 1 );
+    Mat image = imread("/home/liz/MEGAsync/Semestre-II/Imagenes-Loayza/Calibracion/sources/prueba2.png", 1 );
 
     if( !image.data )
     {
@@ -18,63 +20,13 @@ int main( ) {
     return -1;
     }
 
-    Mat gray_image, binary_image, cany;
-    cvtColor( image, gray_image, CV_BGR2GRAY); //CV_BGR2GRAY
-   
-
-    // Canny
-    int erosion_type = MORPH_ELLIPSE;
-    int erosion_size = 1;
-    Mat	kernel =  getStructuringElement( erosion_type,
-                                       Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-                                       Point( erosion_size, erosion_size ) );
-    //Mat erode;
-	//erode(gray_image, gray_image, kernel);
-
+    Mat result = process(image);
     namedWindow( "Original", CV_WINDOW_AUTOSIZE );
-    namedWindow( "Gray image", CV_WINDOW_AUTOSIZE );
-    namedWindow( "Gaussian", CV_WINDOW_AUTOSIZE );
-    namedWindow( "Umbral binary", CV_WINDOW_AUTOSIZE );
-    namedWindow( "Canny", CV_WINDOW_AUTOSIZE );
-    namedWindow( "Erode Cany", CV_WINDOW_AUTOSIZE );
-    namedWindow( "Houg", CV_WINDOW_AUTOSIZE );
 
 
     imshow( "Original", image );
-    imshow( "Gray image", gray_image );
-    GaussianBlur( gray_image, gray_image, Size(15, 15), 2, 2 );
-    imshow(  "Gaussian", gray_image );
-    threshold(gray_image, binary_image, 70, 255, 1); //92 //115
-    imshow(  "Umbral binary", binary_image );
-    Canny(binary_image, cany, 0, 0 * 3, 3);
-    imshow( "Canny", cany );
-    dilate(binary_image, binary_image, kernel);
-    Canny(binary_image, binary_image, 0, 0 * 3, 3);
-    imshow( "Erode Cany", binary_image );
-   
+    imshow( "Procesado", result );
     
-
-    //hough
-    vector<Vec3f> circles;
-    HoughCircles(binary_image, circles, HOUGH_GRADIENT, 1,
-                 binary_image.rows/16,  // change this value to detect circles with different distances to each other
-                 100, 30, 1, 30 // change the last two parameters
-            // (min_radius & max_radius) to detect larger circles
-    );
-    for( size_t i = 0; i < circles.size(); i++ )
-    {
-        Vec3i c = circles[i];
-        Point center = Point(c[0], c[1]);
-        // circle center
-        circle( binary_image, center, 1, Scalar(0,100,100), 3, LINE_AA);
-        // circle outline
-        int radius = c[2];
-        circle( binary_image, center, radius, Scalar(255,0,255), 3, LINE_AA);
-    }
-
-    imshow( "Houg", binary_image );
-    
-
     waitKey(0);
 */
 //Video 
@@ -102,7 +54,7 @@ int main( ) {
     {
         // get the image frame 
         frame = cvQueryFrame( cap );
-  
+        cout << "Image" << endl;
         // exit if unsuccessful
         if( !frame ) break;
          //Grayscale matrix
@@ -130,35 +82,55 @@ Mat process (Mat image)
 
     // Canny
     int erosion_type = MORPH_ELLIPSE;
-    int erosion_size = 2;
+    int erosion_size = 1;
     Mat	kernel =  getStructuringElement( erosion_type,
                                        Size( 2*erosion_size + 1, 2*erosion_size+1 ),
                                        Point( erosion_size, erosion_size ) );
     
     GaussianBlur( result, result, Size(15, 15), 2, 2 );
-    threshold(result, result, 90, 255, 1); //92 //115
+    threshold(result, result, 95, 255, 1); //92 //115  //1 
     dilate(result, result, kernel);
     Canny(result, result, 0, 0 * 3, 3);
     
 
-    //Houg
-     //hough
-    vector<Vec3f> circles;
-    HoughCircles(result, circles, HOUGH_GRADIENT, 1,
-                 1,  // change this value to detect circles with different distances to each other
-                 100, 30, 1, 30 // change the last two parameters
-            // (min_radius & max_radius) to detect larger circles
-    );
-    for( size_t i = 0; i < circles.size(); i++ )
+   vector<vector<Point> > contours;
+   vector<Vec4i> hierarchy;
+   findContours(result, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+   int idx = 0;
+   //circularidad
+   double indexCircularity = 0;
+   double perimeter = 0;
+   double area = 0;
+   //radio
+   double minorAxis = 0;
+   double majorAxis = 0;
+   for( ; idx >= 0; idx = hierarchy[idx][0] )
     {
-        Vec3i c = circles[i];
-        Point center = Point(c[0], c[1]);
-        // circle center
-        circle( image, center, 1, Scalar(0,100,100), 1, LINE_AA);
-        // circle outline
-        int radius = c[2];
-        circle( image, center, radius, Scalar(255,0,255), 1, LINE_AA);
+        //circularidad
+        area = contourArea(contours[idx]);
+        perimeter = arcLength(contours[idx], true);
+        indexCircularity = (4 * PI * area)/pow(perimeter,2);
+        
+        //rectangulos width y heigth debe ser casi iguales
+        //Corregir
+        Rect rect = boundingRect(contours[idx]);
+        if(rect.width > rect.height){
+            majorAxis = rect.width;
+            minorAxis = rect.height;
+        }
+        else if (rect. height > rect.width){
+            majorAxis = rect.height;
+            minorAxis = rect.width;
+        } else{
+            majorAxis = rect.height;
+            minorAxis = rect.width;
+        }
+        
+        //cout << "circularidad: " <<  indexCircularity << " eje mayor: " << minorAxis/ majorAxis;
+        if(indexCircularity > 0.78 && minorAxis/majorAxis > 0.50){
+            drawContours( image, contours, idx,  Scalar(255,0,255), CV_FILLED, 8, hierarchy );
+
+        }
     }
-    return image;
-    //return result;
+   return image;
 }
